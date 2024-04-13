8a4a6fac84edb7186ff36ba45e4d279f69d1ec26
Abdulwajud Ifeoluwa <135984022+CYBERDRONE001@users.noreply.github.com>
2024-04-13 17:51:33 +0100

    Add files via upload

---
+++ Airdropwebsite.js
@@ -0,0 +1,34 @@
+document.getElementById('Button').addEventListener('click', function() {
+    // Redirect to the specified URL
+    window.location.href = "https://legitdogemining.com/?ref=Ifeoluwa001";
+});
+
+document.getElementById('Button1').addEventListener('click', function() {
+    // Redirect to the specified URL
+    window.location.href = "https://dogeboost.pro/?referrer=105414";
+});
+
+document.getElementById('Button2').addEventListener('click', function() {
+    // Redirect to the specified URL
+    window.location.href = "https://t.me/BuzzFollowersbot?start=user11715305";
+});
+
+document.getElementById('Button3').addEventListener('click', function() {
+    // Redirect to the specified URL
+    window.location.href = "https://t.me/BlazenRobot?start=5145669742";
+});
+
+document.getElementById('Button4').addEventListener('click', function() {
+    // Redirect to the specified URL
+    window.location.href = "https://t.me/DigibyteSDGBAirdropBot?start=5145669742";
+});
+
+document.getElementById('Button5').addEventListener('click', function() {
+    // Redirect to the specified URL
+    window.location.href = "https://t.me/BeesLandBot?start=5145669742";
+});
+
+document.getElementById('Button6').addEventListener('click', function() {
+    // Redirect to the specified URL
+    window.location.href = "https://t.me/Tetheruspaybot?start=user11715305";
+});
---
+++ index.js
@@ -0,0 +1,22 @@
+        // script.js
+
+// Define the URLs for the airdrops
+var airdropUrls = {
+    airdrop1: "Crypto Airdrop.html",
+    airdrop2: "Airdropwebsite.html",
+    airdrop3: "https://wa.me/+2347043934781"
+    // Add more URLs as needed
+};
+
+// Add event listener to all airdrop links
+document.addEventListener('DOMContentLoaded', function() {
+    for (var key in airdropUrls) {
+        if (airdropUrls.hasOwnProperty(key)) {
+            document.getElementById(key).addEventListener('click', function(event) {
+                event.preventDefault(); // Prevent default link behavior
+                // Open the corresponding URL in a new tab
+                window.open(airdropUrls[this.id], '_blank');
+            });
+        }
+    }
+});

